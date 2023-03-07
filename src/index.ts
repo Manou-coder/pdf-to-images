100c4231aadfac7546505dd421940b2bbe328584
Manou <manouhaouzi@gmail.com>
2023-03-07 14:41:42 +0200

    fix parcel

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export const age: number = 24
+console.log('age: ', age)
+
+export const prenom: string = 'manou'
+console.log('prenom: ', prenom)
+
+export const salary: number = 3000
+console.log('salary: ', salary)
+
+export const  manou: string[] = ['manou']
+console.log('manou: ', manou)
